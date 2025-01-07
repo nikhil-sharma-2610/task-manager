5d2e10a185a5d328490800d59d3e50a15cd58893
Nikhil Sharma <98894357+nikhil-sharma-2610@users.noreply.github.com>
2025-01-07 16:12:16 +0530

    Update db.ts

--- task-manager-backend/src/db.ts
+++ task-manager-backend/src/db.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 // SQLite connection does not require a username or password
-const sequelize = new Sequelize("task_manager", "root", "Meganikx@0", {
+const sequelize = new Sequelize("task_manager", "root", "password", {
   host: "localhost", // Your MySQL host (use 'localhost' if running locally)
   dialect: "mysql", // MySQL dialect
   logging: false, // Disable logging, can be set to true for debugging
